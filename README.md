2615b70cf91b1b99374cacbca1c20f4de1f143b0
Kieron Browne <kbrowne@pivotal.io>
2019-07-26 09:15:24 +0100

    Add nice endpoint to introduce configurable sleep
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ An app that will use a lot of CPU when asked to do so.
 
 * `/spin` increases the use of CPU
 * `/unspin` decreases the use of CPU
+* `/nice?nanos=<nanos>` makes the process 'nicer' by sleeping each iteration for a number of nanoseconds
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"net/http"
 	"os"
+	"strconv"
 	"sync/atomic"
+	"time"
 )
 
 var spinning int32
+var niceNS int64
 
 func main() {
 	http.HandleFunc("/spin", func(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +20,8 @@ func main() {
 				if isSpinning < myVal {
 					break
 				}
+				nanos := atomic.LoadInt64(&niceNS)
+				time.Sleep(time.Duration(nanos) * time.Nanosecond)
 				// spin!
 			}
 		}()
@@ -24,6 +29,18 @@ func main() {
 
 	http.HandleFunc("/unspin", func(w http.ResponseWriter, r *http.Request) {
 		atomic.AddInt32(&spinning, -1)
+	})
+
+	http.HandleFunc("/nice", func(w http.ResponseWriter, r *http.Request) {
+		nanoVals, ok := r.URL.Query()["nanos"]
+		if !ok || len(nanoVals) < 1 {
+			return
+		}
+		val, err := strconv.Atoi(nanoVals[0])
+		if err != nil {
+			return
+		}
+		atomic.StoreInt64(&niceNS, int64(val))
 	})
 
 	port := os.Getenv("PORT")
